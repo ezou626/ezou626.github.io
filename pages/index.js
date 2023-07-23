d3df3e657a94f2bd5f43cf5e13fa80fcfb032e2b
Eric Zou <ezou626@gmail.com>
2023-07-23 04:37:01 -0400

    its nexting time

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Everything() {
+
+return (
+    <>
+    <div>
+        <h1 font="Roboto">Eric Zou</h1>
+        <h2>Computer Engineering, University of Pennsylvania</h2>
+    </div>
+    </>
+)
+}
