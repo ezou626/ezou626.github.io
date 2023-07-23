f1b0e53edecfc7306eebecf4d46d53f8edcb958a
Eric Zou <ezou626@gmail.com>
2023-07-23 01:19:40 -0400

    Add name to website

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+
+const root = document.getElementById("root");
+
+function HomePage() {
+
+return (
+    <div>
+        <h1>Eric Zou</h1>
+    </div>
+)
+}
+
+ReactDOM.render(<HomePage />, root);
