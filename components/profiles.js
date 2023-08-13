d8ec0bc63f45318b9082e1d65870d389a78de026
Eric Zou <ezou626@gmail.com>
2023-08-13 18:15:35 -0400

    remove unused link

--- components/profiles.js
+++ components/profiles.js
@@ -6,8 +6,6 @@ import githubIcon from '../public/github-icon.svg';
 import gmailIcon from '../public/gmail-icon.svg';
 import linkedinIcon from '../public/linkedin-icon.svg';
 
-const resumelink = 'https://docs.google.com/document/d/1EsoE86d8c6xYKbqX7LTHxdlieA_lAbjB53HeLBha318/edit?usp=sharing'
-
 export default function Profiles() {
     return (
         <footer className='px-7 py-5 text-center animate-revealprofiles'>
