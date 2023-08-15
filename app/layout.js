c9ba2357be4a72e3a796795d13cb9a8e15ea466f
Eric Zou <ezou626@gmail.com>
2023-08-15 16:56:37 -0400

    adding some standard metadata fields

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,18 @@ const roboto = Roboto({
 export const metadata = {
   title: "Eric Zou",
   description: 'My Personal Website',
+  generator: 'Next.js',
+  applicationName: 'Eric Zou\'s Personal Website',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', 'Website'],
+  colorScheme: 'light',
+  creator: 'Eric Zou',
+  publisher: 'Github',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
   icons: []
 }
 
