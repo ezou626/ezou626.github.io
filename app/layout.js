806e25c0e459ee5939d7ea897eb731fdcf17147d
Eric Zou <ezou626@gmail.com>
2023-08-17 01:46:00 -0400

    removed explicit html tags for icons

--- app/layout.js
+++ app/layout.js
@@ -23,10 +23,10 @@ export const metadata = {
     telephone: false,
   },
   icons: {
-    // other: [
-    //   { rel: 'mask-icon' },
-    //   { url: '/mask-icon.svg', type: 'image/svg+xml',  color: "#325758"},
-    // ]
+    other: [
+      { rel: 'mask-icon' },
+      { url: '/safari-pinned-tab.svg', type: 'image/svg+xml'},
+    ]
   }
 }
 
@@ -34,8 +34,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       </head>
       <body className={roboto.className}>
         {children}
