bc0de28d7a74f97354d991ee165844d0d1f34088
Eric Zou <55918461+ezou626@users.noreply.github.com>
2024-03-19 13:09:19 -0400

    Update page.js
    Career description

--- app/about/page.js
+++ app/about/page.js
@@ -16,10 +16,10 @@ return (
         Hi! I'm Eric. I'm a sophomore pursuing a BSE in Computer Science (and hopefully an Accelerated Master's in Computer Science) at the University of Pennsylvania, with the intent to graduate in 2026. I'm interested in software development, AI/ML, and embedded systems as a means to bring cool tech to the general public.
       </p>
       <p className='text-xl'>
-        Right now, I am a student researcher at the Center for Neuroengineering and Therapeutics, using Python, MATLAB, and data analysis packages to analyze fMRI scans for epilepsy diagnosis and treatment. On campus, I'm a member of the Tikkun Olam Makers UPenn chapter, where I write Arduino programs and design circuits to create devices for people in need. I've also participated in hackathons to gain experience with web development, building applications that utilize powerful APIs in innovative ways.
+        Right now, I am a student researcher at the Center for Neuroengineering and Therapeutics, using Python, MATLAB, and data analysis packages to analyze fMRI scans for epilepsy diagnosis and treatment. I've also participated in hackathons to gain experience with web development, building applications that utilize powerful APIs in innovative ways.
       </p>
       <p className='text-xl'>
-        I'm currently searching for a summer 2024 tech-oriented internship to gain industry experience in one of my interests. If you have an open role and think I'd be a great fit, please send me an email to let me know!
+        For summer 2024, I'll be working at Kyowa Kirin NA as an IT/Automation intern.
       </p> 
     </main>
     <Profiles></Profiles>
