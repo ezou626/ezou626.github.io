bd500864cae199ea52a419e60104aba1c4f91f5a
Eric Zou <55918461+ezou626@users.noreply.github.com>
2024-03-19 13:07:41 -0400

    Update page.js
    Change major

--- app/about/page.js
+++ app/about/page.js
@@ -13,7 +13,7 @@ return (
     <main className='h-full px-10 overflow-y-clip space-y-5 py-16 animate-revealbody max-w-5xl mx-auto'>
       <h1 className="text-6xl font-bold">About</h1>
       <p className='text-xl'>
-        Hi! I'm Eric. I'm a sophomore pursuing a BSE in Computer Engineering (and hopefully an Accelerated Master's in Computer Science) at the University of Pennsylvania, with the intent to graduate in 2026. I'm interested in software development, AI/ML, and embedded systems as a means to bring cool tech to the general public.
+        Hi! I'm Eric. I'm a sophomore pursuing a BSE in Computer Science (and hopefully an Accelerated Master's in Computer Science) at the University of Pennsylvania, with the intent to graduate in 2026. I'm interested in software development, AI/ML, and embedded systems as a means to bring cool tech to the general public.
       </p>
       <p className='text-xl'>
         Right now, I am a student researcher at the Center for Neuroengineering and Therapeutics, using Python, MATLAB, and data analysis packages to analyze fMRI scans for epilepsy diagnosis and treatment. On campus, I'm a member of the Tikkun Olam Makers UPenn chapter, where I write Arduino programs and design circuits to create devices for people in need. I've also participated in hackathons to gain experience with web development, building applications that utilize powerful APIs in innovative ways.
