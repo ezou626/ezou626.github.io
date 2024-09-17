19d1892dcffd26ddeb14666413d829443839ac33
Eric Zou <ezou626@gmail.com>
2024-09-16 20:35:51 -0400

    create project page

---
+++ app/projects/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'Projects',
+}
+
+export default function ProjectPage() {
+
+return (
+  <main className='h-full px-10 overflow-y-clip space-y-5 py-16 animate-revealbody max-w-5xl mx-auto'>
+    <h1 className="text-4xl font-bold">Projects</h1>
+  </main>
+)}
