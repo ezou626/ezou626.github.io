4ded787ec1c11b25c5ff2b75bd15a7d889cd9ff5
Eric Zou <ezou626@gmail.com>
2024-09-16 22:05:49 -0400

    change colors and order of labels

--- components/projectBoard.js
+++ components/projectBoard.js
@@ -6,22 +6,12 @@ export default function ProjectBoard({ projects }) {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8">Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.filter(project => project.enable).map((project, index) => (
-          <div key={index} className="bg-white shadow-md rounded-lg p-6">
+          <div key={index} className="shadow-md rounded-lg p-6">
             <h2 className="text-2xl font-bold mb-4">{project.title}</h2>
-            <p className="text-gray-700 mb-4">{project.desc}</p>
-            <div className="mb-4">
-              {project.tools.map((tool, toolIndex) => (
-                <span
-                  key={toolIndex}
-                  className="inline-block bg-gray-200 text-gray-800 text-sm font-semibold mr-2 mb-2 px-3 py-1 rounded"
-                >
-                  {tool}
-                </span>
-              ))}
-            </div>
-            <div className="flex space-x-4 justify-center">
+            <p className="text-gray-500 mb-4">{project.desc}</p>
+            <div className="flex space-x-4 justify-center mb-4">
               {Object.keys(project.links).map((key) => (
                 project.links[key] && (
                   <a
@@ -34,6 +24,16 @@ export default function ProjectBoard({ projects }) {
                     {key.charAt(0).toUpperCase() + key.slice(1)}
                   </a>
                 )
+              ))}
+            </div>
+            <div>
+              {project.tools.map((tool, toolIndex) => (
+                <span
+                  key={toolIndex}
+                  className="inline-block bg-gray-200 text-gray-800 text-sm font-semibold mr-2 mb-2 px-3 py-1 rounded"
+                >
+                  {tool}
+                </span>
               ))}
             </div>
           </div>
