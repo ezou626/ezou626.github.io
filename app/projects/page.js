8cdec99021ad22ec103deef0e998cadc5c056bfa
Eric Zou <ezou626@gmail.com>
2024-09-16 21:27:20 -0400

    add imports for project board

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,3 +1,6 @@
+import ProjectBoard from "../../components/projectBoard"
+import projects from "./projectData"
+
 export const metadata = {
     title: 'Projects',
 }
@@ -6,6 +9,6 @@ export default function ProjectPage() {
 
 return (
   <main className='h-full px-10 overflow-y-clip space-y-5 py-16 animate-revealbody max-w-5xl mx-auto'>
-    <h1 className="text-4xl font-bold">Projects</h1>
+    <ProjectBoard projects={projects}></ProjectBoard>
   </main>
 )}
