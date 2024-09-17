018ceeb569cb5b9bdf5ffa3f414cdb00817cec7d
Eric Zou <ezou626@gmail.com>
2024-09-16 22:27:41 -0400

    data for projects

--- app/projects/projectData.js
+++ app/projects/projectData.js
@@ -12,6 +12,58 @@ const projects = [
         },
         'enable': false,
     },
+    {
+        'title': 'YourKB',
+        'tools': [
+            'React', 'Next.js', 'Supabase', 'Tailwind CSS', 'TypeScript'
+        ],
+        'desc': 'Create and share a creative 1 KB image',
+        'links': {
+            'website': 'https://yourkb.vercel.app',
+            'github': 'https://github.com/ezou626/yourkb',
+            'demo': null
+        },
+        'enable': true,
+    },
+    {
+        'title': 'Coffee Web Blocker',
+        'tools': [
+            'React', 'Web API', 'Chrome API', 'Tailwind CSS', 'Daisy UI', 'TypeScript', 'Webpack'
+        ],
+        'desc': 'Block lists of websites for focus',
+        'links': {
+            'website': 'https://chromewebstore.google.com/detail/coffee-web-blocker/enmnhnecpbfgpbloobjahifddldnoanh?hl=en-US',
+            'github': 'https://github.com/ezou626/coffee-web-blocker',
+            'demo': null
+        },
+        'enable': true,
+    },
+    {
+        'title': 'WalkWithMe',
+        'tools': [
+            'React', 'Google Maps API', 'HTML', 'CSS', 'JavaScript', 'Go', 'Firebase'
+        ],
+        'desc': 'Find people to walk together with',
+        'links': {
+            'website': null,
+            'github': 'https://github.com/ezou626/coffee-web-blocker',
+            'demo': 'https://devpost.com/software/walkwithme-sxw8uh'
+        },
+        'enable': true,
+    },
+    {
+        'title': 'DisCourse',
+        'tools': [
+            'Node.js', 'Discord API', 'TypeScript'
+        ],
+        'desc': 'Educational classroom management Discord bot',
+        'links': {
+            'website': null,
+            'github': 'https://github.com/reantric/DisCourse',
+            'demo': 'https://devpost.com/software/discourse-ni1oe9'
+        },
+        'enable': true,
+    },
 ];
   
 export default projects;
