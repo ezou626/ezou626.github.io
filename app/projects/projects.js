42af34a9153a5c856c287ec939485f5b9b1454a2
Eric Zou <ezou626@gmail.com>
2024-09-16 20:35:38 -0400

    create demo project

---
+++ app/projects/projects.js
@@ -0,0 +1,15 @@
+const projects = [
+    {
+        'title': 'Demo Project',
+        'tools': [
+            'React', 'Next.js', 'Tailwind CSS'
+        ],
+        'desc': 'A really cool project',
+        'links': {
+            'website': '',
+            'github': ''
+        }
+    },
+]
+  
+  export default projects;
