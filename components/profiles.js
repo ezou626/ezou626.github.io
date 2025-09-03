43aac77a1546144c027c124d5b3db50a79f2189d
Eric Zou <ezou626@gmail.com>
2025-09-03 17:32:12 -0400

    refactor profile config for convenience

--- components/profiles.js
+++ components/profiles.js
@@ -6,25 +6,39 @@ import GithubIcon from "../public/github-icon.svg";
 import GmailIcon from "../public/gmail-icon.svg";
 import LinkedinIcon from "../public/linkedin-icon.svg";
 
+const PROFILE_CONFIG = [
+  {
+    name: "Gmail",
+    url: "mailto:ezou626@seas.upenn.edu",
+    icon: GmailIcon,
+  },
+  {
+    name: "Github",
+    url: "https://github.com/ezou626",
+    icon: GithubIcon,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/eric-zou-4512a7219",
+    icon: LinkedinIcon,
+  },
+];
+
 export default function Profiles() {
   return (
     <footer className="px-7 py-5 text-center animate-revealprofiles">
       <ul className="space-x-14 inline-flex">
-        <li>
-          <a href="mailto:ezou626@seas.upenn.edu">
-            <Image src={GmailIcon} className="w-8" alt="Gmail"></Image>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/ezou626">
-            <Image src={GithubIcon} className="w-8" alt="Github"></Image>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/eric-zou-4512a7219">
-            <Image src={LinkedinIcon} className="w-8" alt="Linkedin"></Image>
-          </a>
-        </li>
+        {PROFILE_CONFIG.map((profile) => (
+          <li key={profile.name}>
+            <a href={profile.url}>
+              <Image
+                src={profile.icon}
+                className="w-8"
+                alt={profile.name}
+              ></Image>
+            </a>
+          </li>
+        ))}
       </ul>
     </footer>
   );
