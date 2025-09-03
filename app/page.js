2f7384a0ba79c5ad5da142f05cf6e3d7782a719d
Eric Zou <ezou626@gmail.com>
2025-09-03 18:41:13 -0400

    I'm smarter than i realize

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="h-full overflow-y-clip space-y-5 py-32 animate-revealbody mx-auto max-w-5xl pb-10">
+    <main className="h-full px-10 overflow-y-clip space-y-5 py-32 animate-revealbody mx-auto max-w-5xl pb-10">
       <h1 className="text-6xl font-bold">Eric Zou</h1>
       <h2 className="text-4xl">
         Computer Science BSE Student, University of Pennsylvania
