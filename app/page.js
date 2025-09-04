b99bce467c3c41bda746fb04d9a0d95aa7d8255f
Eric Zou <ezou626@gmail.com>
2025-09-04 00:02:42 -0400

    remove wacky spacing

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="h-full px-10 overflow-y-clip space-y-2 py-32 animate-revealbody mx-auto max-w-5xl pb-10">
+    <main className="h-full px-10 overflow-y-clip space-y-2 animate-revealbody mx-auto max-w-5xl">
       <h1 className="text-6xl font-bold pb-3">Eric Zou</h1>
       <h2 className="text-4xl">Computer Science BSE/MS Student</h2>
       <h2 className="text-4xl">University of Pennsylvania '26</h2>
