bfb3353c5e205b61b396b818d94a1ab126a786a0
Eric Zou <ezou626@gmail.com>
2025-09-03 23:52:17 -0400

    try this on for size

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
-    <main className="h-full px-10 overflow-y-clip space-y-5 py-32 animate-revealbody mx-auto max-w-5xl pb-10">
-      <h1 className="text-6xl font-bold">Eric Zou</h1>
-      <h2 className="text-4xl">Computer Science BSE/MS Student,</h2>
+    <main className="h-full px-10 overflow-y-clip space-y-2 py-32 animate-revealbody mx-auto max-w-5xl pb-10">
+      <h1 className="text-6xl font-bold pb-3">Eric Zou</h1>
+      <h2 className="text-4xl">Computer Science BSE/MS Student</h2>
       <h2 className="text-4xl">University of Pennsylvania '26</h2>
     </main>
   );
