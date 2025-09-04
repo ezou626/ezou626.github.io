fa3fa43c2763cf08872f832b36ad5181bcd572e5
Eric Zou <ezou626@gmail.com>
2025-09-03 23:28:14 -0400

    new look

--- app/page.js
+++ app/page.js
@@ -2,9 +2,8 @@ export default function HomePage() {
   return (
     <main className="h-full px-10 overflow-y-clip space-y-5 py-32 animate-revealbody mx-auto max-w-5xl pb-10">
       <h1 className="text-6xl font-bold">Eric Zou</h1>
-      <h2 className="text-4xl">
-        Computer Science BSE/MS Student, University of Pennsylvania
-      </h2>
+      <h2 className="text-4xl">Computer Science BSE/MS Student,</h2>
+      <h2 className="text-4xl">University of Pennsylvania '26</h2>
     </main>
   );
 }
